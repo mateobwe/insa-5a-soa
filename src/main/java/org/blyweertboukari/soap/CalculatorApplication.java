b3a402cf7a13dada59b13531f870e8db2e8c628b
Yacine BOUKARI <steeveboukari9@gmail.com>
2025-10-03 16:09:57 +0200

    fix(td1): service

--- src/main/java/org/blyweertboukari/soap/CalculatorApplication.java
+++ src/main/java/org/blyweertboukari/soap/CalculatorApplication.java
@@ -5,15 +5,11 @@ import jakarta.xml.ws.Endpoint;
 public class CalculatorApplication {
 
     public final static String host = "localhost";
-    public final static int port1 = 8080;
-    public final static int port2 = 8081;
+    public final static int port = 8080;
 
     public void startService() {
-        String url1 = "http://"+host+":"+port1+"/";
-        String url2 = "http://"+host+":"+port2+"/";
-        CalculatorService service = new CalculatorService();
-        Endpoint.publish(url1, service);
-        Endpoint.publish(url2, service);
+        String url = "http://" + host + ":" + port + "/";
+        Endpoint.publish(url, new CalculatorService());
     }
 
     public static void main(String[] args) {
--- src/main/java/org/blyweertboukari/soap/CalculatorService.java
+++ src/main/java/org/blyweertboukari/soap/CalculatorService.java
@@ -3,25 +3,30 @@ package org.blyweertboukari.soap;
 import jakarta.jws.WebService;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebParam;
+import jakarta.jws.WebResult;
 
 @WebService(serviceName="Calculator")
 public class CalculatorService {
-    @WebMethod(operationName="Add")
+    @WebMethod(operationName="Add", action="http://tempuri.org/Add")
+    @WebResult(name = "AddResult")
     public int add(@WebParam(name="intA") int a, @WebParam(name="intB") int b) {
         return a + b;
     }
 
-    @WebMethod(operationName="Subtract")
+    @WebMethod(operationName="Subtract", action = "http://tempuri.org/Subtract")
+    @WebResult(name="SubtractResult")
     public int subtract(@WebParam(name="intA") int a, @WebParam(name="intB") int b) {
         return a - b;
     }
 
-    @WebMethod(operationName="Multiply")
+    @WebMethod(operationName="Multiply", action = "http://tempuri.org/Multiply")
+    @WebResult(name="MultiplyResult")
     public int multiply(@WebParam(name="intA") int a, @WebParam(name="intB") int b) {
         return a * b;
     }
 
-    @WebMethod(operationName="Divide")
+    @WebMethod(operationName="Divide", action = "http://tempuri.org/Divide")
+    @WebResult(name="DivideResult")
     public int divide(@WebParam(name="intA") int a, @WebParam(name="intB") int b) {
         return a / b;
     }
